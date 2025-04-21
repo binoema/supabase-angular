a2b187f96a77b40ace8f6522babda3eaedcca863
Marc Binöder <marc.binoeder@gmx.de>
2025-04-21 20:37:15 +0200

    adjust app component to show other components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core'
+import { SupabaseService } from './supabase/supabase.service'
+import { AccountComponent } from "./account/account.component";
+import { AuthComponent } from "./auth/auth.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [AccountComponent, AuthComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'supabase-angular';
+export class AppComponent implements OnInit {
+    title = 'angular-user-management'
+    private readonly supabase = inject(SupabaseService)
+    public session = this.supabase.session
+
+    ngOnInit() {
+        this.supabase.authChanges((_, session) => (this.session = session))
+    }
 }
